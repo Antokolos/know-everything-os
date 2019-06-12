6081b74cf44b5ea90a13f569cb519a851dcd8700
Antokolos <antokolos@gmail.com>
2019-06-12 15:32:28 +0600

    Added auto-updating of available games list on game join screen

--- scenes/game_join.gd
+++ scenes/game_join.gd
@@ -1,10 +1,15 @@
 extends TextureRect
 
+const MAX_TICKS = 20
+
 # SQLite module
 const SQLite = preload("res://lib/gdsqlite.gdns")
 
 signal data_updated(lobby_ID)
 
+onready var update_controls_node = get_node("VBoxContainer/HBoxUpdateControls")
+onready var update_controls_label_node = update_controls_node.get_node("Label")
+onready var update_timer_node = get_node("UpdateTimer")
 onready var lobbies_grid_node = get_node("VBoxContainer/VBoxLobbies/HBoxLobbies/ScrollContainer/LobbiesGrid")
 onready var player_node = get_node("VBoxContainer/VBoxLobbies/LobbiesHeader/Player")
 onready var categories_node = get_node("VBoxContainer/VBoxLobbies/LobbiesHeader/Categories")
@@ -14,6 +19,8 @@ onready var wait_message_node = get_node("VBoxContainer/VBoxLobbies/MessageWait"
 onready var back_node = get_node("VBoxContainer/HBoxActions/Back")
 
 var friends_only = false
+var had_available_games = true
+var ticks_left = MAX_TICKS
 var db = null
 var all_cats_size
 
@@ -46,16 +53,26 @@ func _on_lobby_data_update(success, steamIDLobby, steamIDMember):
 	if success:
 		emit_signal("data_updated", steamIDLobby)
 
+func clear_lobbies_grid():
+	for ch in lobbies_grid_node.get_children():
+		if self.is_connected("data_updated", ch, "_on_data_updated"):
+			self.disconnect("data_updated", ch, "_on_data_updated")
+		ch.queue_free()
+
 func update_list(lobby_list):
-	for child in lobbies_grid_node.get_children():
-		child.queue_free()
+	clear_lobbies_grid()
 	
 	if lobby_list.empty():
 		wait_message_node.text = tr("GAME_JOIN_NO_GAMES_MESSAGE")
 		player_node.visible = false
 		categories_node.visible = false
+		had_available_games = false
+		update_timer_node.start()
 		return
 	else:
+		if not had_available_games:
+			$HasGame.play()
+		had_available_games = true
 		wait_message_node.text = ""
 		
 	player_node.visible = true
@@ -80,6 +97,7 @@ func update_list(lobby_list):
 		child_separator.text = ""
 		lobbies_grid_node.add_child(child_separator)
 		godotsteam.request_lobby_data(lobby_ID)
+	update_timer_node.start()
 
 func _on_Back_pressed():
 	close()
@@ -93,14 +111,34 @@ func _on_lobby_connect_pressed(lobby_ID):
 	close()
 	godotsteam.connect_to_lobby(lobby_ID)
 
-func _on_ButtonFriendsOnly_toggled(button_pressed):
-	friends_only = button_pressed
+func request_lobbies():
+	update_timer_node.stop()
+	update_controls_node.visible = false
+	ticks_left = MAX_TICKS
 	if friends_only:
 		godotsteam.request_friends_lobby_list()
 	else:
 		godotsteam.request_lobby_list()
 
+func _on_ButtonFriendsOnly_toggled(button_pressed):
+	friends_only = button_pressed
+	request_lobbies()
+
+func _on_Button_pressed():
+	request_lobbies()
+
+func _on_UpdateTimer_timeout():
+	update_controls_node.visible = true
+	ticks_left = ticks_left - 1
+	if ticks_left <= 0:
+		ticks_left = MAX_TICKS
+		request_lobbies()
+		return
+	update_controls_label_node.text = tr("GAME_JOIN_UPDATE_IN") % ticks_left
+	update_timer_node.start()
+
 func close():
+	update_timer_node.stop()
 	if db:
 		# Close database
 		db.close()
